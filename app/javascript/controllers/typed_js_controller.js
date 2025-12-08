e6d4c0c9dadfbbf60b73c9afb669898db98d0f8c
Felippe Menezes <felippemenezes1982@gmail.com>
2025-12-08 14:42:11 -0300

    Update typed text

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -3,9 +3,22 @@ import Typed from "typed.js"
 
 // Conecta-se a data-controller="typed-js"
 export default class extends Controller {
+  initialize() {
+    this.boundStartTyped = this.startTyped.bind(this);
+  }
+
   connect() {
+    document.addEventListener("turbo:load", this.boundStartTyped);
+    this.startTyped(); // Para a primeira carga da página
+  }
+
+  disconnect() {
+    document.removeEventListener("turbo:load", this.boundStartTyped);
+  }
+
+  startTyped() {
     new Typed(this.element, {
-      strings: ["Is a web application for managing sports teams, players, matches, and fantasy sports operations."],
+      strings: ["It's a web application for managing soccer teams, players, playing friendly matches and exclusive championships."],
       typeSpeed: 40,
       loop: false,
       showCursor: false
